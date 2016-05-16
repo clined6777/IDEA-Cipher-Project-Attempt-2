5ff6fa83d5283c4d825d65f7f93575b7d302c6da
clined6777 <ClineD6777@sduhsd.lan>
2016-05-16 14:49:55 -0700

    Encryptor semi-fixes

--- IDEA/src/Encryptor.java
+++ IDEA/src/Encryptor.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -13,35 +14,41 @@ public class Encryptor implements Ciphering{
 	public Encryptor(String text, String keySet) {
 		plainText = text;
 		key = keySet;
+		cipherText = "";
 		//if (keySet.length() != 8) {
 		//	System.out.println("Keys must be long in order to proceed");
 		//	return;
 		//}
 		byteKey = keySet.getBytes();
+		byteKey = Arrays.copyOf(byteKey, 16);
 		subKeys = new short[52];
 		byte[] tempKey = byteKey;
-		for (int k = 0; k < 8; k++) {
+		for (int k = 0; k < 6; k++) {
 			subKeys[8 * k] = (short) (byteKey[0] << 8 + (int) byteKey[1]);
-			subKeys[8 * k + 1] = (short) (byteKey[2] << 8 + (int) byteKey[3]);
-			subKeys[8 * k + 2] = (short) (byteKey[4] << 8 + (int) byteKey[5]);
-			subKeys[8 * k + 3] = (short) (byteKey[6] << 8 + (int) byteKey[7]);
-			subKeys[8 * k + 4] = (short) (byteKey[8] << 8 + (int) byteKey[9]);
-			subKeys[8 * k + 5] = (short) (byteKey[10] << 8 + (int) byteKey[11]);
-			subKeys[8 * k + 6] = (short) (byteKey[12] << 8 + (int) byteKey[13]);
-			subKeys[8 * k + 7] = (short) (byteKey[14] << 8 + (int) byteKey[15]);
+			subKeys[8 * k + 1] = (short) ((byteKey[2] << 8) + (int) byteKey[3]);
+			subKeys[8 * k + 2] = (short) ((byteKey[4] << 8) + (int) byteKey[5]);
+			subKeys[8 * k + 3] = (short) ((byteKey[6] << 8) + (int) byteKey[7]);
+			subKeys[8 * k + 4] = (short) ((byteKey[8] << 8) + (int) byteKey[9]);
+			subKeys[8 * k + 5] = (short) ((byteKey[10] << 8) + (int) byteKey[11]);
+			subKeys[8 * k + 6] = (short) ((byteKey[12] << 8) + (int) byteKey[13]);
+			subKeys[8 * k + 7] = (short) ((byteKey[14] << 8) + (int) byteKey[15]);
 			for (int g = 0; g < byteKey.length; g++)
-				tempKey[g] = (byte) (byteKey[(g + 3) % byteKey.length] << 1 + byteKey[(g + 4) % byteKey.length] >> 7);
+				tempKey[g] = (byte) (((byteKey[(g + 3) % byteKey.length]) << 1) + ((byteKey[(g + 4) % byteKey.length]) >> 7));
 			byteKey = tempKey;
+//			for (int g = 0; g < 8*k+7; g++)
+//				byteKey[g] = tempKey[g];
 		}
+		for (int k = 0; k < 4; k++)
+			subKeys[48 + k] = (short) ((byteKey[2 * k] << 8) + (int) (byteKey[2 * k + 1]));
 		byte[] tempByte = text.getBytes();
 		if (tempByte.length % 8 != 0) {
 			int spaces = 8 - (tempByte.length % 8);
 			byteText = new byte[tempByte.length + spaces];
 			for (int i = 0; i < tempByte.length; i++)
 				byteText[i] = tempByte[i];
-			for (int j = tempByte.length; j < byteText.length; j += 2) {
-				byteText[j] = 3;
-				byteText[j + 1] = 2;
+			for (int j = tempByte.length; j < byteText.length; j++) {
+				byteText[j] = 32;
+				//byteText[j + 1] = 2;
 			}
 		}
 		else
@@ -50,6 +57,7 @@ public class Encryptor implements Ciphering{
 	
 	public Encryptor(File fileText) {
 		try {
+			cipherText = "";
 			Scanner file = new Scanner(fileText);
 			key = file.nextLine();
 			while (file.hasNextLine()) {
@@ -58,7 +66,7 @@ public class Encryptor implements Ciphering{
 			byteKey = key.getBytes();
 			subKeys = new short[52];
 			byte[] tempKey = byteKey;
-			for (int k = 0; k < 8; k++) {
+			for (int k = 0; k < 6; k++) {
 				subKeys[8 * k] = (short) (byteKey[0] << 8 + (int) byteKey[1]);
 				subKeys[8 * k + 1] = (short) (byteKey[2] << 8 + (int) byteKey[3]);
 				subKeys[8 * k + 2] = (short) (byteKey[4] << 8 + (int) byteKey[5]);
@@ -71,6 +79,8 @@ public class Encryptor implements Ciphering{
 					tempKey[g] = (byte) ((byteKey[(g + 3) % byteKey.length] << 1) % 256 + byteKey[(g + 4) % byteKey.length] >> 7);
 				byteKey = tempKey;
 			}
+			for (int k = 0; k < 4; k++)
+				subKeys[48 + k] = (short) (byteKey[2 * k] << 8 + (int) byteKey[2 * k + 1]);
 			byte[] tempByte = plainText.getBytes();
 			if (tempByte.length % 8 != 0) {
 				int spaces = 8 - (tempByte.length % 8);
@@ -96,12 +106,15 @@ public class Encryptor implements Ciphering{
 		if (key.length() == 8)
 			System.out.print("You cannot encrypt with this key. Please enter a key with 8 characters.");
 		else {
+			System.out.println(byteKey.length);
+			System.out.println(Arrays.toString(subKeys));
+			System.out.println(Arrays.toString(byteText));
 			short[] shortToHex = new short[byteText.length / 2];
 			for (int i = 0; i < byteText.length / 8; i++) {
-				short s1 = (short) (byteText[8 * i] << 8 + (int) byteText[8 * i + 1]);
-				short s2 = (short) (byteText[8 * i + 2] << 8 + (int) byteText[8 * i + 3]);
-				short s3 = (short) (byteText[8 * i + 4] << 8 + (int) byteText[8 * i + 5]);
-				short s4 = (short) (byteText[8 * i + 6] << 8 + (int) byteText[8 * i + 7]);
+				short s1 = (short) ((byteText[8 * i] << 8) + (int) byteText[8 * i + 1]);
+				short s2 = (short) ((byteText[8 * i + 2] << 8) + (int) byteText[8 * i + 3]);
+				short s3 = (short) ((byteText[8 * i + 4] << 8) + (int) byteText[8 * i + 5]);
+				short s4 = (short) ((byteText[8 * i + 6] << 8) + (int) byteText[8 * i + 7]);
 				for (int j = 0; j < 8; j++) {
 					short multMod1 = multMod(s1, subKeys[6 * j]);
 					short addMod1 = addMod(s2, subKeys[6 * j + 1]);
@@ -148,14 +161,25 @@ public class Encryptor implements Ciphering{
 	}
 	
 	public short multMod(short sh1, short sh2) {
-		return (short) ((sh1 * sh2) % (65536 + 1));
+		int output = 0;
+		if (sh1 != 0 && sh2 != 0)
+			output = (sh1 * 65536) & 0xFFFF;
+		else if (sh1 == 0)
+			output = (65536 * sh2) & 0xFFFF;
+		else if (sh2 == 0)
+			output = (sh1 * 65536) & 0xFFFF;
+		else if (sh1 == 0 && sh2 == 0)
+			output = (65536 * 65536) & 0xFFFF;
+		if (output == 65536)
+			output = 0;
+		return (short) output;
 	}
 	
 	public void setKey(String keySet) {
 		key = keySet;
 		byteKey = keySet.getBytes();
 		byte[] tempKey = byteKey;
-		for (int k = 0; k < 8; k++) {
+		for (int k = 0; k < 6; k++) {
 			subKeys[8 * k] = (short) (byteKey[0] << 8 + (int) byteKey[1]);
 			subKeys[8 * k + 1] = (short) (byteKey[2] << 8 + (int) byteKey[3]);
 			subKeys[8 * k + 2] = (short) (byteKey[4] << 8 + (int) byteKey[5]);
@@ -168,6 +192,8 @@ public class Encryptor implements Ciphering{
 				tempKey[g] = (byte) ((byteKey[(g + 3) % byteKey.length] << 1) % 256 + byteKey[(g + 4) % byteKey.length] >> 7);
 			byteKey = tempKey;
 		}
+		for (int k = 0; k < 4; k++)
+			subKeys[48 + k] = (short) (byteKey[2 * k] << 8 + (int) byteKey[2 * k + 1]);
 	}
 	
 	public String getKey() {
@@ -179,6 +205,7 @@ public class Encryptor implements Ciphering{
 	}
 	
 	public String getCipherText() {
+		System.out.println(Arrays.toString(cipherText.getBytes()));
 		return cipherText;
 	}
 }
